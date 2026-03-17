fdf665edfb9fd26e948a775c455e019620e32194
juanmagarcia88 <garciafloresjuanma@gmail.com>
2026-03-17 18:38:18 +0100

    Create landing page with background, text and images

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,25 @@
+import background from '../assets/pokemon-wallpaper.jpg'
+import reactIcon from '../assets/react-icon.png'
+import pokedex from '../assets/pokedex.png'
+import './Landing.css'
+
 export default function Landing() {
     return (
-        <h1>Landing</h1>
+        <main
+            className="landing"
+            style={{ backgroundImage: `url(${background})` }}
+        >
+
+            <div className='landing-content'>
+                <div className='images'>
+                    <img src={reactIcon} alt="Icono de react"></img>
+                    <img src={pokedex} alt="Pokedex"></img>
+                </div>
+
+                <div className='landing-h1'>
+                    <h1 className='react'>React</h1><h1 className='pokedex'>Pokedex</h1>
+                </div>
+            </div>
+        </main>
     )
 }
