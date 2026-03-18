448d794be6d56cd64ff205f762dc03bdc5c1b04d
juanmagarcia88 <garciafloresjuanma@gmail.com>
2026-03-18 12:18:23 +0100

    Fetch and display pokemon names

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,15 @@
+import { useState, useEffect } from 'react'
 import './Home.css'
 
 export default function Home() {
+
+    const [allPokemons, setAllPokemons] = useState([])
+
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+            .then(response => response.json())
+            .then(data => setAllPokemons(data.results))
+    }, [])
 
     return (
         <main>
@@ -9,14 +18,21 @@ export default function Home() {
                     <h1 className='home-react'>React</h1><h1 className='home-pokedex'>Pokedex</h1>
                 </div>
 
-                <label className="searcher" for="searcher">Search pokemon by name: 
-                    <input id="searcher" type="text"/>
+                <label className="searcher">Search pokemon by name:
+                    <input id="searcher" type="text" />
                 </label>
             </div>
 
-            <div>
+            <div className='cards'>
+
+                {allPokemons.map((poke, index) => (
+                    <div className='card' key={index}>
+                        <h2>{poke.name}</h2>
+                    </div>
+                ))}
 
             </div>
+
         </main>
     )
 }
