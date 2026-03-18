498cd148d10675c8d7ad5f597d7479036ee3439e
juanmagarcia88 <garciafloresjuanma@gmail.com>
2026-03-18 11:52:36 +0100

    Add topbar with title and provisional search in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,22 @@
+import './Home.css'
+
 export default function Home() {
+
     return (
-        <h1>Home</h1>
+        <main>
+            <div className="top-bar">
+                <div className='home-landing-h1'>
+                    <h1 className='home-react'>React</h1><h1 className='home-pokedex'>Pokedex</h1>
+                </div>
+
+                <label className="searcher" for="searcher">Search pokemon by name: 
+                    <input id="searcher" type="text"/>
+                </label>
+            </div>
+
+            <div>
+
+            </div>
+        </main>
     )
 }
