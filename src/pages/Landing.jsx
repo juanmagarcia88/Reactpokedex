f68124f3717d8207987f1d800392492629309db0
juanmagarcia88 <garciafloresjuanma@gmail.com>
2026-03-18 10:56:04 +0100

    Add enter button with route navigation and landing page styles

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import background from '../assets/pokemon-wallpaper.jpg'
 import reactIcon from '../assets/react-icon.png'
 import pokedex from '../assets/pokedex.png'
+import { Link } from 'react-router-dom'
 import './Landing.css'
 
 export default function Landing() {
@@ -11,14 +12,26 @@ export default function Landing() {
         >
 
             <div className='landing-content'>
+                <a href='https://github.com/juanmagarcia88' target='_blank'>
+                    <p>Create by: juanmagarcia88</p>
+                </a>
                 <div className='images'>
-                    <img src={reactIcon} alt="Icono de react"></img>
-                    <img src={pokedex} alt="Pokedex"></img>
+                    <a href='https://es.react.dev/' target='_blank'>
+                        <img src={reactIcon} alt="Icono de react"></img>
+                    </a>
+                    <a href='https://www.pokemon.com/es/pokedex' target='_blank'>
+                        <img src={pokedex} alt="Pokedex"></img>
+                    </a>
                 </div>
 
                 <div className='landing-h1'>
                     <h1 className='react'>React</h1><h1 className='pokedex'>Pokedex</h1>
                 </div>
+
+                <Link to="/home">
+                    <button>Enter</button>
+                </Link>
+
             </div>
         </main>
     )
