bb6dbd991263df10299209daa89a5fe1f99d40bb
juanmagarcia88 <garciafloresjuanma@gmail.com>
2026-03-25 13:10:48 +0100

    Create and integrate Header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { PokemonCard } from '../components/PokemonCard'
 import { PokemonModal } from '../components/PokemonModal'
 import { Pagination } from '../components/Pagination'
+import { Header } from '../components/Header'
 import './Home.css'
 
 export default function Home() {
@@ -117,49 +118,15 @@ export default function Home() {
 
     return (
         <main>
-            <div className="top-bar">
-                <Link to="/">
-                    <div className='home-landing-h1'>
-                        <h1 className='home-react'>React</h1>
-                        <h1 className='home-pokedex'>Pokedex</h1>
-                    </div>
-                </Link>
-
-                <div className='filters'>
-                    <label className="searcher">
-                        Search pokemon by name:
-                        <input onChange={(e) => setSearch(e.target.value)} id="searcher" type="text" />
-                    </label>
-                    <select className='types' onChange={(e) => setType(e.target.value)}>
-                        <option value="">All types</option>
-                        <option value="normal">Normal</option>
-                        <option value="fire">Fire</option>
-                        <option value="water">Water</option>
-                        <option value="grass">Grass</option>
-                        <option value="electric">Electric</option>
-                        <option value="ice">Ice</option>
-                        <option value="fighting">Fighting</option>
-                        <option value="poison">Poison</option>
-                        <option value="ground">Ground</option>
-                        <option value="rock">Rock</option>
-                        <option value="flying">Flying</option>
-                        <option value="psychic">Psychic</option>
-                        <option value="bug">Bug</option>
-                        <option value="ghost">Ghost</option>
-                        <option value="dragon">Dragon</option>
-                        <option value="dark">Dark</option>
-                        <option value="steel">Steel</option>
-                        <option value="fairy">Fairy</option>
-                    </select>
-                </div>
-
-                <div>
-                    <h2 className='favorites-h2' onClick={() => setShowFavs(!showFavs)}>
-                        {showFavs ? "All Pokemon" : "Favorites ⭐"}
-                    </h2>
-                </div>
-
-            </div>
+            
+            <Header
+                setSearch={setSearch}
+                setType={setType}
+                setShowFavs={setShowFavs}
+                showFavs={showFavs}
+                search={search}
+                type={type}
+            />
 
             {!isFiltering && (
                 <Pagination
